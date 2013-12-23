a27ce43cad96af2d79469ac310d4b6fe3ffdc334
dai <dai@ncrypthic.webkita.web.id>
2013-12-23 16:57:38 +0700

    * Add no child process error exception

---
+++ src/Ncrypthic/Daemon/Exception/NoChildProcessErrorException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Ncrypthic\Daemon\Exception;
+
+/**
+ * Decription of ChildProcessErrorException
+ *
+ * created : Dec 23, 2013 4:22:01 PM
+ * @author Lim Afriyadi <adi@whatiwear.com>
+ */
+class NoChildProcessErrorException extends \Exception
+{
+}
